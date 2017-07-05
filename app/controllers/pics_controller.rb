2a4e52062d1d90459a1eec4bb8cc1b790e90fb6a
home <home@homes-MBP-3.att.net>
2017-07-05 00:41:15 -0700

    Add content to pics controller with strong params

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,29 +1,49 @@
 class PicsController < ApplicationController
 	def index
-		
+		@pics = Pic.all
 	end
 
 	def show
-		
+		@pic = Pic.find(params[:id])
 	end
 
 	def new
-		
+		@pic = Pic.new
 	end
 
 	def create
-		
+		@pic = Pic.new(pic_params)
+		if @pic.save
+			redirect_to @pic.haircut
+		else
+			@errors = @pic.errors.full_messages
+			render "new"
+		end
 	end
 
 	def edit
-		
+
 	end
 
 	def update
-		
+		if @pic.update(pic_params)
+			flash[:success] = "Pic updated."
+			redirect_to @pic.haircut
+		else
+			@errors = @pic.errors.full_messages
+			render "edit"
+		end
 	end
 
 	def destroy
-		
+		@pic = Pic.find(params[:id])
+		haircut = @pic.haircut
+		@pic.destroy
+		redirect_to haircut
 	end
+
+	private
+  def pic_params
+    params.require(:pic).permit(:haircut_id, :title, :url, :body)
+  end
 end
