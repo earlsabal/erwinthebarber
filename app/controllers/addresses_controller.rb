09615717ac9a2eccef93181826848f040d7fbd67
earlsabal <earlsabal@gmail.com>
2017-08-21 13:19:45 -0700

    Implement Address controller

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,3 +1,21 @@
 class AddressesController < ApplicationController
+	def edit
+		@address = Address.find(params[:id])
+	end
 
+	def update
+		@address = Address.find(params[:id])
+		if @address.update(address_params)
+			flash[:success] = "Address updated."
+			redirect_to addresss_path
+		else
+			@errors = @address.errors.full_messages
+			render "edit"
+		end
+	end
+
+	private
+	def address_params
+		params.require(:address).permit(:name, :number, :street, :city, :state)
+	end
 end
