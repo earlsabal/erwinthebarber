d20720ff693a4e87e8a99406d56e7c6e9533b943
earlsabal <earlsabal@gmail.com>
2017-08-21 13:30:00 -0700

    Create routes for controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
+
   devise_for :admins, :skip => [:registrations] 
   root to:"pages#home"
 
+  resources :addresses, only: [:edit, :update]
+  resources :buttons, only: [:edit, :update]
+  resources :descriptions, only: [:edit, :update]
+  resources :informations, only: [:edit, :update]
+  resources :services, only: [:edit, :update]
   resources :haircuts, only: [:index, :new, :create, :edit, :update, :destroy] do
   	resources :pics
   end
 
 	get '/archives', to: 'pics#index'
+	
 end
