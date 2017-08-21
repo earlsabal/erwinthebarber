d01ba28ebb23796f7866ff3a43003672049b2d12
earlsabal <earlsabal@gmail.com>
2017-08-21 13:50:43 -0700

    Update application helper and contact partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,15 @@
 module ApplicationHelper
 
+	def phone_info
+		Information.find_by(name: "phone")
+	end
+
 	def find_phone
 		Information.find_by(name: "phone").body
+	end
+
+	def email_info
+		Information.find_by(name: "email")
 	end
 
 	def find_email
