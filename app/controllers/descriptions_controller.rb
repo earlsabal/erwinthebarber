216360d0b8a7858c85dc1d629c80d3be32519694
earlsabal <earlsabal@gmail.com>
2017-08-21 13:23:50 -0700

    Add content to descriptions controller

--- app/controllers/descriptions_controller.rb
+++ app/controllers/descriptions_controller.rb
@@ -1,3 +1,23 @@
 class DescriptionsController < ApplicationController
 
+	def edit
+		@description = Description.find(params[:id])
+	end
+
+	def update
+		@description = Description.find(params[:id])
+		if @description.update(description_params)
+			flash[:success] = "Description updated."
+			redirect_to descriptions_path
+		else
+			@errors = @description.errors.full_messages
+			render "edit"
+		end
+	end
+
+	private
+	def description_params
+		params.require(:description).permit(:name, :title, :body)
+	end
+
 end
