34bec83b7f149639fa0c0a348896ae65aa959e9f
earlsabal <earlsabal@gmail.com>
2017-08-21 13:26:13 -0700

    Add content to informations controller

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,3 +1,21 @@
 class ServicesController < ApplicationController
+	def edit
+		@service = Service.find(params[:id])
+	end
 
+	def update
+		@service = Service.find(params[:id])
+		if @service.update(service_params)
+			flash[:success] = "Service updated."
+			redirect_to services_path
+		else
+			@errors = @service.errors.full_messages
+			render "edit"
+		end
+	end
+
+	private
+	def service_params
+		params.require(:service).permit(:title, :body, :icon)
+	end
 end
