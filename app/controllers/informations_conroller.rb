e10e51ba52e44bb1aed629ecd1458842a4612500
earlsabal <earlsabal@gmail.com>
2017-08-21 13:25:04 -0700

    Add content to informations controller

--- app/controllers/informations_conroller.rb
+++ app/controllers/informations_conroller.rb
@@ -1,3 +1,23 @@
 class InformationsController < ApplicationController
 
+	def edit
+		@information = Information.find(params[:id])
+	end
+
+	def update
+		@information = Information.find(params[:id])
+		if @information.update(information_params)
+			flash[:success] = "Information updated."
+			redirect_to informations_path
+		else
+			@errors = @information.errors.full_messages
+			render "edit"
+		end
+	end
+
+	private
+	def information_params
+		params.require(:information).permit(:name, :body)
+	end
+
 end
