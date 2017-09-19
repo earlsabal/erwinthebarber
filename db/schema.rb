406c5b3ee0e970b1d183dc1e26549273e2e1874c
earlsabal <earlsabal@gmail.com>
2017-09-18 23:21:54 -0700

    Update form for pic to implement paperclip

--- db/schema.rb
+++ db/schema.rb
@@ -72,7 +72,10 @@ ActiveRecord::Schema.define(version: 20170821065732) do
   end
 
   create_table "pics", force: :cascade do |t|
-    t.string "url"
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
     t.string "title"
     t.text "body"
     t.bigint "haircut_id"
