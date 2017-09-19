7a34c33850102348849e786089fee7a5e5310d8e
earlsabal <earlsabal@gmail.com>
2017-09-19 03:28:18 -0700

    Update produuction configs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
   config.paperclip_defaults = {
     storage: :s3,
     s3_protocol: 'http',
-    s3_host_name: 'us-east-1.amazonaws.com',
+    s3_host_name: 's3-us-east-1.amazonaws.com',
     s3_credentials: {
       bucket: ENV['AWS_S3_BUCKET'],
       access_key_id: ENV['AWS_ACCESS_KEY_ID'],
